9aee5483790fb3c662f9a4e9a51753138e9dcc1f
chrispark000 <chrispark000@gmail.com>
2021-10-08 10:00:29 -0400

    adding test for ghost move

--- Projects/P2/tests/TestGhostMove.java
+++ Projects/P2/tests/TestGhostMove.java
@@ -1,10 +1,17 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import static org.junit.Assert.*;
 
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		Mainframe frame = new MainFrame();
+		frame.addGhost(new Location(9,11), "test_ghost", Color.red);
+		WallComponent wall = new WallComponent(9,10,frame.scale);
+		myMap.add("wall", loc, wall, Map.Type.WALL);
+		
+		// should be surrounded by walls, hence no possible move
+		assertFalse(frame.ghosts.get(0).move());
 	}
 }

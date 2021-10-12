232656ea5c83eb444fb96e467ae7004078313084
Vibhor <vibhorg16799@gmail.com>
2021-10-11 22:43:46 -0400

    Test function added

--- Projects/P2/tests/TestPacManValidMoves.java
+++ Projects/P2/tests/TestPacManValidMoves.java
@@ -1,10 +1,30 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		
+		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y
+
+		//alternatively if you don't need the PacMan or Ghost objects in your tests
+		frame.initPlayers(); //Creates all of the players
+
+		//Start The Game
+		frame.startGame();
+		Map myMap = frame.getMap();
+
+		ArrayList<Location> arr = new ArrayList<>();
+		arr.add(1,2);
+		
+		// should be surrounded by walls, hence no possible move
+		assertFalse(arr);
+
+
 	}
 }

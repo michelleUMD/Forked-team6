f5bdc1036b122c3e6544d8d2cd7d46da0f796175
Kashif Peshimam <kashif.peshimam9@gmail.com>
2021-10-11 20:44:15 -0400

    pacman-consume tests

--- Projects/P2/tests/TestConsume.java
+++ Projects/P2/tests/TestConsume.java
@@ -5,6 +5,20 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		//Creating A Map
+		// Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+		//Creating Players
+		// Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
+
+		//alternatively if you don't need the PacMan or Ghost objects in your tests
+		// frame.initPlayers(); //Creates all of the players
+
+		//Start The Game
+		frame.startGame();
+
+		assertTrue(pacman.consume() != null);	
 	}
 }

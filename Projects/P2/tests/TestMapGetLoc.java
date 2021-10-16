ca33d817f1d576da39737dac556272ef36da08d5
Kashif Peshimam <kashif.peshimam9@gmail.com>
2021-10-15 23:50:35 -0400

    test fixes

--- Projects/P2/tests/TestMapGetLoc.java
+++ Projects/P2/tests/TestMapGetLoc.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import java.io.*;
 
 
-public class TestMapGetLoc {
+public class TestMapGetLoc extends TestCase {
 	
 	public void testMapGetLoc() throws FileNotFoundException{
 		MainFrame frame = new MainFrame();
@@ -11,8 +11,8 @@ public class TestMapGetLoc {
 		frame.addPacMan(new Location(9,10));
 		frame.startGame();
 		// should be surrounded by walls, hence no possible move
-		Assert.assertEquals(frame.getMap().getLoc(new Location(9,11)), Map.Type.GHOST);
-		Assert.assertEquals(frame.getMap().getLoc(new Location(0,0)), Map.Type.WALL);
-		Assert.assertEquals(frame.getMap().getLoc(new Location(9,10)), Map.Type.PACMAN);
+		assertEquals(frame.getMap().getLoc(new Location(9,11)), Map.Type.GHOST);
+		assertEquals(frame.getMap().getLoc(new Location(0,0)), Map.Type.WALL);
+		assertEquals(frame.getMap().getLoc(new Location(9,10)), Map.Type.PACMAN);
 	}
 }
